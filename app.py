ea194a56d9fea5d3346cbb5de3b02767cd1bd0e2
MiguelAntonio <200932574@my.xu.edu.ph>
2023-11-17 11:14:44 +0800

    3rd commit
    -changed dices to have more faces, from 1 dice with 4 faces to another dices with 20 faces
    -added extra checks for die types in requests
    -added extra validation for get request to check correct die type.
    -added extra validations for the post request to check correct die type

--- app.py
+++ app.py
@@ -1,32 +1,74 @@
 from flask import Flask, jsonify, request
 import random
 
+
 app = Flask(__name__)
 
-#route to get the random dice roll
-@app.route('/api/dice', methods=['GET'])
-def get_dice_roll():
-    dice_faces = [1, 2, 3, 4, 5, 6]
-    random_roll = random.choice(dice_faces)
-    return jsonify({'dice_roll': random_roll})
+#route to get the random dice roll for a specified die
+@app.route('/api/dice/<die_type>', methods=['GET'])
+def get_die_roll(die_type):
+    #available dice types
+    valid_die_types = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20']
 
-#route to post number of dice rolled
+    #to check if the die is valid
+    if die_type not in valid_die_types:
+        return jsonify({'message': f'Invalid die type. Valid types are the ff: {", ".join(valid_die_types)}'}), 400
+
+    
+    if die_type == 'd4':
+        die_faces = [1, 2, 3, 4]
+    elif die_type == 'd6':
+        die_faces = [1, 2, 3, 4, 5, 6]
+    elif die_type == 'd8':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8]
+    elif die_type == 'd10':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    elif die_type == 'd12':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    elif die_type == 'd20':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+    random_roll = random.choice(die_faces)
+
+    return jsonify({'die_type': die_type, 'die_roll': random_roll})
+
+#route to post number of specified dice rolled
 # "num_rolls" : int between 1 and 6
-@app.route('/api/dice', methods=['POST'])
-def post_dice_rolls():
+@app.route('/api/dice/<die_type>', methods=['POST'])
+def post_die_rolls(die_type):
     data = request.get_json()
 
+    #to check if the die is valid
+    valid_die_types = ['d4', 'd6', 'd8', 'd10', 'd12', 'd20']
+    if die_type not in valid_die_types:
+        return jsonify({'message': f'Invalid die type. Valid types: {", ".join(valid_die_types)}'}), 400
+
+    if die_type == 'd4':
+        die_faces = [1, 2, 3, 4]
+    elif die_type == 'd6':
+        die_faces = [1, 2, 3, 4, 5, 6]
+    elif die_type == 'd8':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8]
+    elif die_type == 'd10':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    elif die_type == 'd12':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    elif die_type == 'd20':
+        die_faces = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+    #check if proper request body is sent
     if 'num_rolls' not in data:
-        return jsonify({'message': 'Cannot find "num_rolls" parameter'}), 400
+        return jsonify({'message': 'Cannot find "num_rolls" request body parameter'}), 400
 
     num_rolls = data['num_rolls']
 
     if not isinstance(num_rolls, int) or num_rolls <= 0:
-        return jsonify({'message': 'Please input a positive number.'}), 400
+        return jsonify({'message': 'Invalid num_rolls value. It should be a positive integer.'}), 400
 
-    dice_rolls = [random.choice([1, 2, 3, 4, 5, 6]) for _ in range(num_rolls)]
+    #show the random dice rolls in list
+    dice_rolls = [random.choice(die_faces) for _ in range(num_rolls)]
 
-    return jsonify({'dice_rolls': dice_rolls})
+    return jsonify({'die_type': die_type, 'dice_rolls': dice_rolls})
 
 if __name__ == '__main__':
     app.run(debug=True)
